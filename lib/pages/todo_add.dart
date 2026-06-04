066dd4ec4989bcea18f4674f89f64da77a643273
rikutota <taka010218@gmail.com>
2026-06-04 21:04:09 +0900

    feat : リスト追加画面をStatefulWidgetに変更し、テキスト入力機能を追加

--- lib/pages/todo_add.dart
+++ lib/pages/todo_add.dart
@@ -1,18 +1,67 @@
 import "package:flutter/material.dart";
 
-class TodoAdd extends StatelessWidget {
+class TodoAdd extends StatefulWidget {
   const TodoAdd({super.key});
+
+  @override
+  State<TodoAdd> createState() => _TodoAddState();
+}
+
+class _TodoAddState extends State<TodoAdd> {
+  String _Text = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: const Text('リスト追加画面（クリックで戻る）'),
-        ),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("リスト追加"),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(64.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_Text,
+            style: const TextStyle(
+              fontSize: 24,
+            ),
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                hintText: "リストの内容を入力してください",
+              ),
+              onChanged: (String value) {
+                setState(() {
+                  _Text = value;
+                });
+              },
+            ),
+            const SizedBox(height: 8.0),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                },
+                child: const Text("リスト追加",
+                style: TextStyle(
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+            ),
+            const SizedBox(height: 8.0),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("キャンセル"),
+              ),
+            ),
+          ],
+          )
       ),
     );
   }
